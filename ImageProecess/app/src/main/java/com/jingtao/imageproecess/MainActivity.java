caa80cc64713ec1163ecb8d663d85fd931c772b1
Jing Tao <genetao06@hotmail.com>
2015-10-25 19:44:29 -0400

    drawing update(have bug)

--- ImageProecess/app/src/main/java/com/jingtao/imageproecess/MainActivity.java
+++ ImageProecess/app/src/main/java/com/jingtao/imageproecess/MainActivity.java
@@ -4,12 +4,17 @@ import android.app.Activity;
 import android.content.ContentResolver;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.graphics.Path;
 import android.graphics.Point;
 import android.graphics.PointF;
 import android.graphics.Rect;
 import android.graphics.RectF;
+import android.graphics.drawable.BitmapDrawable;
 import android.media.Image;
 import android.net.Uri;
 import android.provider.MediaStore;
@@ -45,6 +50,7 @@ public class MainActivity extends Activity {
     static final int DRAW =3;
     int mode = NONE;
 
+
     // Remember some things for zooming
     PointF start = new PointF();
     PointF mid = new PointF();
@@ -57,6 +63,8 @@ public class MainActivity extends Activity {
     private float height;
     private float width;
     private RectF viewRect;
+    private int displaywidth;
+    private int displayheight;
     private PointF last = new PointF();
 
     //variable for counting two successive up-down events
@@ -68,6 +76,15 @@ public class MainActivity extends Activity {
     //constant for defining the time duration between the click that can be considered as double-tap
     static final int MAX_DURATION = 500;
 
+    private Path drawPath;
+    //drawing and canvas paint
+    private Paint drawPaint, canvasPaint;
+    //initial color
+    private int paintColor = 0xFF660000;
+    //canvas
+    private Canvas canvas;
+    //canvas bitmap
+    private Bitmap canvas_bitmap;
 
     @Override
     public void onWindowFocusChanged(boolean hasFocus) {
@@ -107,6 +124,13 @@ public class MainActivity extends Activity {
         marker_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Bitmap workingBipmap = ((BitmapDrawable)myimage.getDrawable()).getBitmap();
+                canvas_bitmap = workingBipmap.copy(Bitmap.Config.ARGB_8888, true);
+                canvas = new Canvas(canvas_bitmap);
+                drawPath = new Path();
+                drawPaint = new Paint();
+                setPaint();
+                myimage.setOnTouchListener(image_draw);
                 hand_btn.setBackgroundColor(Color.parseColor("#00000000"));
                 marker_btn.setBackgroundColor(Color.parseColor("#2928dd"));
             }
@@ -121,6 +145,43 @@ public class MainActivity extends Activity {
             }
         });
     }
+    View.OnTouchListener image_draw = new View.OnTouchListener() {
+        @Override
+        public boolean onTouch(View v, MotionEvent event){
+            try{
+                Log.e("info", "draw:raw event.X: "+event.getRawX()+" event.Y: " +event.getRawY());
+                float touchX = event.getX()*((float)canvas_bitmap.getWidth()/(float)displaywidth);
+                float touchY = event.getY()*((float)canvas_bitmap.getHeight()/(float)displayheight);
+                Log.e("info", "draw: X: "+touchX+" Y: " +touchY);
+                Log.e("info", "draw: imagewidth: " + myimage.getWidth() + " imageheight: " +myimage.getHeight());
+
+                //respond to down, move and up events
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        drawPath.moveTo(touchX, touchY);
+                        break;
+                    case MotionEvent.ACTION_MOVE:
+                        drawPath.lineTo(touchX, touchY);
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        drawPath.lineTo(touchX, touchY);
+                        canvas.drawPath(drawPath, drawPaint);
+                        myimage.setImageBitmap(canvas_bitmap);
+                        drawPath.reset();
+                        break;
+                    default:
+                        return false;
+                }
+            //Log.e("draw",drawPath.toString());
+            //redra
+            myimage.draw(canvas);
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+
+            return true;
+        }
+    };
 
     View.OnTouchListener image_scale = new View.OnTouchListener() {
         @Override
@@ -213,7 +274,6 @@ public class MainActivity extends Activity {
             return true; // indicate event was handled
         }
     };
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -258,8 +318,8 @@ public class MainActivity extends Activity {
         Display display = getWindowManager().getDefaultDisplay();
         Point size = new Point();
         display.getSize(size);
-        int displaywidth = size.x;
-        int displayheight = size.y;
+        displaywidth = size.x;
+        displayheight = size.y;
         Log.e(TAG, "globalX: " + globalX + " globalY:" + globalY);
         Log.e(TAG, "img width: " + imgwidth + " img height:" + imgheight);
         if(globalX< -imgwidth/2){
@@ -288,16 +348,12 @@ public class MainActivity extends Activity {
             case 0://pick from gellary
                 if(resultCode == RESULT_OK){
                     Uri selectedImage = intent.getData();
-                    Bitmap bitmap;
                     Bitmap bitmap_large;
                     try {
                         bitmap_large = MediaStore.Images.Media.getBitmap(this.getContentResolver(), selectedImage);
-                        bitmap = Bitmap.createScaledBitmap(bitmap_large, 450, 450, true);
                         myimage.setImageBitmap(bitmap_large);
                         matrix = new Matrix(myimage.getImageMatrix());
                         savedMatrix = new Matrix(myimage.getImageMatrix());
-                        height = myimage.getDrawable().getIntrinsicHeight()+20;
-                        width = myimage.getDrawable().getIntrinsicWidth()+20;
                         fix();
                     } catch (Exception e) {
                         Log.e("exception", e.toString()+" OnActivityResult");
@@ -305,6 +361,16 @@ public class MainActivity extends Activity {
                 }
                 break;
         }
+    }
+
+    private void setPaint(){
+        drawPaint.setColor(paintColor);
+        drawPaint.setAntiAlias(true);
+        drawPaint.setDither(true);
+        drawPaint.setStyle(Paint.Style.STROKE);
+        drawPaint.setStrokeJoin(Paint.Join.ROUND);
+        drawPaint.setStrokeCap(Paint.Cap.ROUND);
+        drawPaint.setStrokeWidth(10);
     }
 
 }
